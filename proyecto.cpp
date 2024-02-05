1dd41b345add76fd54cbd8d0bae3d6cf1fe18e76
QuantyumX <izivenuz@gmail.com>
2024-02-05 15:33:38 -0500

    actualizado

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,47 +1,75 @@
 #include <iostream>
 using namespace std;
 
-//IMPLEMENTACION DE ESTRUCTURA DE DATOS
-struct ListaSimple{ //!Lista de Platos
+//DECLARACION  DE ESTRUCTURA DE DATOS
+struct ListaSimple1{ //!Lista de Platos
     string nombre;
     string calorias;
     double precio;
 
-    ListaSimple* sig;
-} plato;
+    ListaSimple1* sig;
+} *plato;
 
-struct Cola{ //! Cola de pedidos durante el dia;
+struct ListaSimple2{ //! Lista de datos del DIA para la inversion.
+    string nombreDia;
+    int ingresos;
+    int ganancias;
 
-} pedido;
+    ListaSimple2* sig;
+} *infoDia;
 
-struct Pila{ //! Pila de platos hechos durante la semana.
-    
-} historial;
+struct Pila{ //! Pila de pedidos durante el dia y semana.
+    string nombreCliente;
+    string edad;
+    string pedido;
+    int compra;
 
-ListaSimple* CrearLista(){
+    Pila* sig;
+} *pedido;
 
-}
-Cola* CrearCola(){
+//! Aca deberia declararse para el stock de platos, pero la verdad nose como hcerlo, si solo con un array o algun estructura de datos xd si tienen en mente una avisan.
 
-}
-Pila* CrearPila(){
-    
-}
+
+//AGREGAR LAS OPERACIONES PARA CADA ESTRUCTURA DE DATOS.
+
+
+
+
 
 
 int main(){
+    plato = NULL, pedido = NULL, infoDia = NULL;
     string dia[] = { "LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO" };
     int i = 1, j; 
     char opc;
     while(true){
-        cout<<"*************** Semana "<<i<<" ***************: \n";
+        cout<<"                                  *************** Semana "<<i<<" *************** \n";
         int k = 0;
         for (j = 0; j<7; j++){
-            cout<<" --------- Dia "<<dia[k]<<"---------  :\n ";
-
+            cout<<"                                       --------- Dia "<<dia[k]<<"---------  \n ";
             k++;
+            while (true){
+                    cout<<"\n";
+					cout << "               				[!INGRESA   TU   EDAD!]                              "<<endl;
+			        cout << endl;	
+					cout << "	------------------------------------------------------------------------------------------------------"<<endl;
+					cout << "	| Mira las recomendaciones que te damos de los platillos  con sus respectivas calorias segun tu edad. |"<<endl;
+					cout << "	-------------------------------------------------------------------------------------------------------"<<endl;
+					cout << endl;
+					cout << "Primero ingresa tu EDAD:  "; //ESTE COUT ESTARA DENTRO DE LA FUNCION INSERTAR(push) para la pila
+					//Se llena el campo de la pila.
+					cout << endl;
+					cout << "Finalmente ingresa tu NOMBRE:  "; //ESTE COUT ESTARA DENTRO DE LA FUNCION INSERTAR(push) para la pila
+                    // se llena el campo de la pila.
+					cout << endl;
+					cout << endl;
+                    cout<<" *Siguiente cliente? (S/N)? \n";
+                    cin>>opc;
+                    if (opc == 'N' || opc == 'n'){
+                        break;
+                    }   
+            }
         }
-        
 
         i++;
         cout<<"Siguiente semana (S/N)? \n";
